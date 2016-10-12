9777351035bf8150f971eff4785dfa5050d6bcce
Kaleb Bickmore <kbic17@gmail.com>
2016-10-12 04:50:33 +0000

    webpage is now finished and all set up.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+document.getElementById("twitter").onclick=function(){
+window.location.replace("https//:twitter.com"); 
+}
+document.getElementById("insta").onclick=function(){
+window.location.href="https//:instagram.com"; 
+}
+document.getElementById("facebook").onclick=function(){
+window.top.location.href="https//:facebook.com"; 
+}
+document.getElementById("contactUs").onclick=//function(){
 
+// window.location.href="/contact.html"; 
+//}
+function myMove() {
+  var elem = document.getElementById("soda");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+     // elem.style.left = pos + 'px';
+    }
+  }
+}
